04f02f830625f8c4f4b26dafcba87505ae02e8b9
engi2nee <engi2nee@gmail.com>
2020-06-28 19:14:03 +0300

    fixed binary search when value doesn’t exist

--- src/searchalgorithms/binarysearch/BinarySearch.kt
+++ src/searchalgorithms/binarysearch/BinarySearch.kt
@@ -2,8 +2,9 @@ package searchalgorithms.binarysearch
 
 fun <T : Comparable<T>> ArrayList<T>.binarySearch(value: T, range: IntRange = indices): Int {
 
+    val size = range.last - range.first + 1
     val middle = (range.last + range.first).ushr(1) //prevent stack overflow
-
+    if(size <= 1 && this[middle]!= value) return  -1
     return when{
         this[middle] == value -> middle
         this[middle] > value -> binarySearch(value, range.first until middle)
